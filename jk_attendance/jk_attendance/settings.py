f7dc26413494ded595d62452821deda79e932546
nidhisd <nidhidhammani@gmail.com>
2019-02-15 23:08:25 -0800

    MOving wsgi.py and renaming port environment variable for Heroku

--- jk_attendance/jk_attendance/settings.py
+++ jk_attendance/jk_attendance/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': os.environ.get("USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
         'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT"),
+        'PORT': os.environ.get("DATABASE_PORT"),
 
     }
 }
